5d3510c736f762095b16ffb07a8c41d4edeb3bed
Eluspa <eluspa23@gmail.com>
2022-04-05 09:13:49 +0200

    040422

---
+++ buttons/generazione buttons.ps1
@@ -0,0 +1,23 @@
+﻿$template = 'C:\Users\PC-UCNET\Desktop\organica\buttons\buttontemplate.txt'
+$newpath = 'C:\Users\PC-UCNET\Desktop\organica\buttons\buttons.txt'
+$contenuto = Get-Content -Path $template
+
+$folder = 'ox'
+
+$img = 'C:\Users\PC-UCNET\Desktop\organica\img\'
+$imgfolders = Get-ChildItem -Path $img
+
+Remove-Item $newpath
+
+for($i=1; $i -le 30;$i++)
+{
+    $contenuto -replace '@', $i -creplace 'FOLDER', $folder | Add-Content -Path $newpath
+}
+
+foreach ($imgfld in $imgfolders)
+{
+    New-Item -ItemType Directory -Name $folder -Path ($img + $imgfld)
+}
+
+
+pause
